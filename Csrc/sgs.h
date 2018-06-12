b1217514d95fc05f8ab01bae4f74ebd021ce9b20
Ignacio <ilopezgo@caltech.edu>
2018-06-11 17:34:53 -0700

    Dev of AMD sgs

--- Csrc/sgs.h
+++ Csrc/sgs.h
@@ -17,6 +17,23 @@ double* restrict buoy_freq, double* restrict strain_rate_mag, double cs, double 
     return;
 }
 
+void amd_update(const struct DimStruct *dims, double* restrict visc, double* restrict diff,
+double* restrict buoy_freq, double* restrict strain_rate, double cs, double prt){
+
+    double delta = cbrt(dims->dx[0]*dims->dx[1]*dims->dx[2]);
+    double scaled_delta = delta*sqrt(1.0/3.0);
+
+    for (ssize_t i=0; i<dims->npg; i++){
+        visc[i] = cs*cs*delta*delta*strain_rate_mag[i];
+        if(buoy_freq[i] > 0.0){
+            double fb = sqrt(fmax(1.0 - buoy_freq[i]/(prt*strain_rate_mag[i]*strain_rate_mag[i]),0.0));
+            visc[i] = visc[i] * fb;
+        }
+        diff[i] = visc[i]/prt;
+    }
+    return;
+}
+
 
 void smagorinsky_update_wall(const struct DimStruct *dims, double* restrict zl_half, double* restrict visc,
 double* restrict diff,double* restrict buoy_freq, double* restrict strain_rate_mag, double cs, double prt){
